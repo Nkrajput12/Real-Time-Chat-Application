cb75e879dcdb3101003eba2754aeab79ff3c60f4
Nkrajput12 <nand.kishor_cs22@gla.ac.in>
2026-05-10 13:22:05 +0530

    Intial commit

---
+++ connecthub-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://apigateway-vg9j.onrender.com', // Replace with your Render ApiGateway URL
+  hubUrl: 'https://apigateway-vg9j.onrender.com/hubs/chat' // Proxied through ApiGateway
+};
